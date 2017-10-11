839053352740ded379d1a11c147091fcd2a9fc2f
Jon Glover <jglover.space@gmail.com>
2017-10-10 20:39:22 -0400

    Allow user to specify the bot they want to test

--- index.js
+++ index.js
@@ -8,7 +8,19 @@
 const virgil       = require('./virgil.js');
 const discord_chan = require('./discord-chan.js');
 
+const params = process.argv[2];
+
 // Initialize the Discord Bot
 console.log("Starting Gem Exchange bots.");
-virgil.initBot();
-discord_chan.initBot();
+
+switch(params){
+  case "virgil":
+    virgil.initBot();
+    break;
+  case "discordchan":
+    discord_chan.initBot();
+    break;
+  default:
+    virgil.initBot();
+    discord_chan.initBot();
+}
